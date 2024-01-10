ea4146669766a948c2d93369fe0a0657f4d42a4f
USER <USER@DESKTOP-LQV4VHB>
2024-01-10 08:44:53 +0530

    second commit

---
+++ src/main/java/inheritance/HierarchicalChildA.java
@@ -0,0 +1,19 @@
+package inheritance;
+
+public class HierarchicalChildA extends  HierarchicalParent{
+	
+	
+public void sub() {
+		
+		System.out.println(a-b);
+	}
+
+	
+
+	public static void main(String[] args) {
+		HierarchicalChildA obj=new HierarchicalChildA();
+				obj.sub();
+
+	}
+
+}
